a5deaa2b6e5ff08d0af9c941e5b0e55539a4991c
Yair Weizman <yairweizmannyw@gmail.com>
2026-05-05 14:17:17 +0300

    -Added Animal Functions (Missing sense , act , eat)

--- src/ecosystem/entities/LivingEntity.java
+++ src/ecosystem/entities/LivingEntity.java
@@ -47,4 +47,47 @@ public class LivingEntity extends AbstractEntity implements Actable
        if(m_energy <= 0)
            this.setIs_alive(false);
     }
+
+    // ===================== Getters & Setters =====================
+
+
+    public double getM_energy() {
+        return m_energy;
+    }
+
+    public boolean setM_energy(double m_energy)
+    {
+        if(m_energy < 0)
+            return false;
+
+        this.m_energy = m_energy;
+        return true;
+    }
+
+    public double getM_maxEnergy() {
+        return m_maxEnergy;
+    }
+
+    public boolean setM_maxEnergy(double m_maxEnergy)
+    {
+        if(m_maxEnergy < 0)
+            return false;
+
+        this.m_maxEnergy = m_maxEnergy;
+        return true;
+    }
+
+    public double getM_age()
+    {
+        return m_age;
+    }
+
+    public boolean setM_age(double m_age)
+    {
+        if(m_age < 0)
+            return false;
+
+        this.m_age = m_age;
+        return true;
+    }
 }
--- src/ecosystem/entities/animals/Animal.java
+++ src/ecosystem/entities/animals/Animal.java
@@ -1,13 +1,35 @@
 package ecosystem.entities.animals;
 
+import ecosystem.behaviors.FeedingBehavior;
+import ecosystem.behaviors.MovementStrategy;
 import ecosystem.core.Environment;
+import ecosystem.core.Position;
 import ecosystem.entities.AbstractEntity;
+import ecosystem.entities.LivingEntity;
 import ecosystem.interfaces.*;
 
 import java.util.List;
 
-public class Animal implements Movable , Eater , EdibleByCarnivore, Sensory
+@SuppressWarnings("all")
+public abstract class Animal extends LivingEntity implements Movable , Eater ,
+        EdibleByCarnivore, Sensory , Consumable
 {
+
+    // ===================== FIELDS =====================
+    private FeedingBehavior m_eatBehavior;
+    private MovementStrategy m_animalMove;
+    private int m_visionRange;
+
+    // ===================== Constructors =====================
+    public Animal(Position position, char symbol , boolean is_alive, double energy , double maxEnergy , double age ,
+                  FeedingBehavior eatBehavior, MovementStrategy animalMove)
+    {
+        super(position,symbol , is_alive , energy , maxEnergy , age);
+        this.m_eatBehavior = eatBehavior;
+        this.m_animalMove = animalMove;
+        this.m_visionRange = 2;
+    } //Animal Constructor
+
 
 
     //Interface Methods
@@ -28,8 +50,36 @@ public class Animal implements Movable , Eater , EdibleByCarnivore, Sensory
 
     //Sensory
     @Override
-    public List<AbstractEntity> sense(Environment env) // get all Entities in Distance
+    public List<AbstractEntity> sense(Environment env) // get all Entities in Distance // Later
     {
       return null;
     }
+
+    //Consumable
+    @Override
+    public double getNutritionValue()
+    {
+      return 0.8f * this.getM_energy();
+    }
+
+    @Override
+    public void onConsumed()
+    {
+        this.setIs_alive(false);
+    }
+
+    //Actable (Father)
+    @Override
+    public void act(Environment env )
+    {
+        super.act(env); //Father Acts (Removing Energy By 2 , age +1 , checks if Energy Below Zero)
+        sense(env); // Checks for NearBy Entities
+
+
+
+
+    }
+
+
+
 }
