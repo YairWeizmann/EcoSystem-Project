1f263c2583fc04a7156d4f00d5b3a02937e5777a
Yair Weizman <yairweizmannyw@gmail.com>
2026-05-05 15:40:27 +0300

    -Added Flower.java (reproduce not finished) -Added OakTree.java (reproduce not finished) -Added Plant.java (Methods)

--- src/ecosystem/entities/plants/Flower.java
+++ src/ecosystem/entities/plants/Flower.java
@@ -1,6 +1,25 @@
 package ecosystem.entities.plants;
 
-public class Flower
-{
+import ecosystem.core.Environment;
+import ecosystem.core.Position;
 
+@SuppressWarnings("all")
+
+public class Flower extends Plant
+{
+    // ===================== Constructors =====================
+    public Flower(Position position, char symbol, boolean is_alive, double energy, double max_energy, double age,
+                  double growthRate, double reproductionChance)
+    {
+        super(position, symbol, is_alive, energy, max_energy = 70, age, growthRate = 5, reproductionChance = 0.2f);
+    }
+
+    // ===================== Interfaces Methods =====================
+    //Reproduceable
+    @Override
+    public boolean reproduce(Environment env)
+    {
+        //Makes a new flowers (1-3) in a distance of 2 from this current Flower
+        return false;
+    }
 }
--- src/ecosystem/entities/plants/OakTree.java
+++ src/ecosystem/entities/plants/OakTree.java
@@ -1,4 +1,32 @@
 package ecosystem.entities.plants;
 
-public class OakTree {
+import ecosystem.core.Environment;
+import ecosystem.core.Position;
+
+@SuppressWarnings("all")
+public class OakTree extends Plant
+{
+
+    // ===================== FIELDS =====================
+
+
+
+    // ===================== Constructors =====================
+    public OakTree(Position position, char symbol, boolean is_alive, double energy, double max_energy, double age, double growthRate, double reproductionChance)
+    {
+        super(position, symbol, is_alive, energy = 80f, max_energy = 120, age, growthRate = 2, reproductionChance = 0.05f);
+    }
+
+    // ===================== Interfaces Methods =====================
+    //Reproducible
+    @Override
+    public boolean reproduce(Environment env)
+    {
+        //New Slot in Distance 1 from the current Oak Tree
+
+        return true;
+    }
+
+
+
 }
